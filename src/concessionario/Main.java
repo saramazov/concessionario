a25a3ee0d706a1b5f4bdfe4caa2ce52bf0def257
Alessio d'Arienzo <alessio.darienzo1092@gmail.com>
2019-12-07 12:59:53 +0100

    Buon Week-end

--- src/concessionario/Main.java
+++ src/concessionario/Main.java
@@ -16,44 +16,22 @@ import concessionario.database.TableDipendente;
 import concessionario.database.TableVeicolo;
 
 public class Main {
-	
+
 	static Scanner input = new Scanner(System.in);
 	static Integer cid = 0;
 	static DriverInterface<Veicolo> driverV = new DriverMemoryVeicolo("prova.csv");
 	static DriverInterface<Dipendente> driverD = new DriverMemoryDipendente("prova2.csv");
 	static TableVeicolo tableV = new TableVeicolo(driverV);
 	static TableDipendente tableD = new TableDipendente(driverD);
-	
+
 	static Veicolo v = new Auto(null, null, null, null, null, null, null);
-	
 
-	//static Dipendente d = new Dipendente(null, null, null, null);
-	
-
+	// static Dipendente d = new Dipendente(null, null, null, null);
 
 	public static void main(String[] args) {
-		Dipendente d = new Dipendente(221,"Luca","Mosciatti",false); 
-		tableD.update(d);
-		//System.out.println(tableV.get("NV867FD").getTarga());
-		System.out.println(d.getCid());
-		//System.out.println(tableD.get(221).getCid());
-		//Collection<Dipendente> dipi =  tableD.get();
-		//for(Dipendente di:dipi) {
-			//System.out.println(di.toString());
-		//}
-		tableV.update(new Auto("NV867FD","autobianchi","34","porpora","diesel",1.0,2));
-		
-		
-		// table.delete("c");
-		// table2.delete(123);
-
-		// table.insert(new Auto("d", "fiat", "panda", "diesel", "giallo", 200.50, 3));
-		// table2.insert(new Dipendente(123, "luha", "rossi", false));
-
-		
 
 		System.out.println("******BENVENUTO NELLA NOSTRA CONCESSIONARIA !!!! ******");
-		System.out.println("Sei un cliente o un Dipendente?\n1. Cliente\n2.Dipendente");
+		System.out.println("\nSei un cliente o un Dipendente?\n1. Cliente\n2.Dipendente\n");
 		int choice = input.nextInt();
 		switch (choice) {
 		case 1:
@@ -61,7 +39,6 @@ public class Main {
 			break;
 		case 2:
 
-			
 			descrivi();
 
 			break;
@@ -71,56 +48,59 @@ public class Main {
 	}
 
 	public static void descrivi() {
-		
+
 		System.out.println("Inserisci CID: ");
-	
+
 		cid = input.nextInt();
 		Dipendente dip;
 		dip = tableD.get(cid);
-		
+
 		if (dip.getAmministratore()) {
 			String uscita = "n";
-			while(uscita.equals("n") || uscita.equals("N")) {
-			System.out.println("Benvenuto "+dip.getNome()+ " "+dip.getCognome()+":\nSei un amministratore !");
-			System.out.println("\n1. Visualizza Veicoli\n2.Visualizza Dipendenti\n3.Modifica Veicoli\n4.Modifica Personale");
-			int scelta=0;
-			scelta = input.nextInt();
-			switch(scelta) {
-			case 1:
-				Collection <Veicolo>veicoli =tableV.get();
-				for(Veicolo v:veicoli) {
-					v.descrivi();
-				}
-				break;
-			case 2: 
-				Collection <Dipendente>dipendenti=tableD.get();
-				for(Dipendente d:dipendenti) {
-					if(d.getAmministratore() == false) System.out.println(d.toString());
-				}
-				
-				break;
-			case 3: 
-				modificaVeicoli();
-				break;
-			case 4:
-				modificaPersonale();
-				break;
-				default: 
+			while (uscita.equals("n") || uscita.equals("N")) {
+				System.out
+						.println("Benvenuto " + dip.getNome() + " " + dip.getCognome() + ":\nSei un amministratore !");
+				System.out.println(
+						"\n1. Visualizza Veicoli\n2.Visualizza Dipendenti\n3.Modifica Veicoli\n4.Modifica Personale");
+				int scelta = 0;
+				scelta = input.nextInt();
+				switch (scelta) {
+				case 1:
+					Collection<Veicolo> veicoli = tableV.get();
+					for (Veicolo v : veicoli) {
+						v.descrivi();
+					}
+					break;
+				case 2:
+					Collection<Dipendente> dipendenti = tableD.get();
+					for (Dipendente d : dipendenti) {
+						if (d.getAmministratore() == false)
+							System.out.println(d.toString());
+					}
+
+					break;
+				case 3:
+					modificaVeicoli();
+					break;
+				case 4:
+					modificaPersonale();
+					break;
+				default:
 					System.out.println("SELEZIONA UNA DELLE 4 POSSIBILITA'");
 					break;
+				}
+
+				System.out.println("\nVuoi uscire dal menu? [Y/N]");
+				uscita = input.next();
 			}
-			
-			System.out.println("Vuoi uscire dal menu? [Y/N]");
-			uscita = input.next();
-			}
-			
+
 		}
-		
+
 	}
 
-		public static void modificaVeicoli() {
-			String uscita = "n";
-			while(uscita.equals("n") || uscita.equals("N")) {
+	public static void modificaVeicoli() {
+		String uscita = "n";
+		while (uscita.equals("n") || uscita.equals("N")) {
 			System.out.println("\n1.Inserisci nuovo veicolo\n2.Elimina veicolo (venduto)");
 			int scelta = 0;
 			scelta = input.nextInt();
@@ -144,7 +124,7 @@ public class Main {
 					Double prezzo = input.nextDouble();
 					System.out.println("Inserisci numero portiere: ");
 					Integer numeroPorte = input.nextInt();
-					tableV.insert(new Auto(targa,produttore,modello,alimentazione,colore,prezzo,numeroPorte));
+					tableV.insert(new Auto(targa, produttore, modello, alimentazione, colore, prezzo, numeroPorte));
 					break;
 				case "Auto":
 					System.out.println("Inserisci Targa: ");
@@ -161,7 +141,7 @@ public class Main {
 					prezzo = input.nextDouble();
 					System.out.println("Inserisci numero portiere: ");
 					numeroPorte = input.nextInt();
-					tableV.insert(new Auto(targa,produttore,modello,alimentazione,colore,prezzo,numeroPorte));
+					tableV.insert(new Auto(targa, produttore, modello, alimentazione, colore, prezzo, numeroPorte));
 					break;
 				case "moto":
 					System.out.println("Inserisci Targa: ");
@@ -178,7 +158,7 @@ public class Main {
 					prezzo = input.nextDouble();
 					System.out.println("Inserisci altezza sedile: ");
 					Double altezzaSedile = input.nextDouble();
-					tableV.insert(new Moto(targa,produttore,modello,alimentazione,colore,prezzo,altezzaSedile));
+					tableV.insert(new Moto(targa, produttore, modello, alimentazione, colore, prezzo, altezzaSedile));
 					break;
 				case "Moto":
 					System.out.println("Inserisci Targa: ");
@@ -194,12 +174,13 @@ public class Main {
 					System.out.println("Inserisci prezzo: ");
 					prezzo = input.nextDouble();
 					System.out.println("Inserisci numero portiere: ");
-					altezzaSedile= input.nextDouble();
-					tableV.insert(new Moto(targa,produttore,modello,alimentazione,colore,prezzo,altezzaSedile));
+					altezzaSedile = input.nextDouble();
+					tableV.insert(new Moto(targa, produttore, modello, alimentazione, colore, prezzo, altezzaSedile));
 					break;
-					default: System.out.println("SELEZIONA UNA DELLE 2 POSSIBILITA'");
+				default:
+					System.out.println("SELEZIONA UNA DELLE 2 POSSIBILITA'");
 				}
-				
+
 				break;
 			case 2:
 				System.out.println("Inserisci la targa del veicolo che vuoi eliminare: ");
@@ -207,108 +188,114 @@ public class Main {
 				Boolean finder = tableV.delete(targa);
 				if (finder == false) {
 					System.out.println("Il veicolo non esiste!");
-				}
-				else System.out.println("Il veicolo è stato eliminato!");
+				} else
+					System.out.println("Il veicolo è stato eliminato!");
 				break;
-				default:
-					System.out.println("SELEZIONA UNA DELLE 4 POSSIBILITA'");
-					break;
+			default:
+				System.out.println("SELEZIONA UNA DELLE 4 POSSIBILITA'");
+				break;
 			}
-			
+
 			System.out.println("Hai finito di modificare veicoli? [Y/N]");
 			uscita = input.next();
-			
-			}
-}
-		
-		public static void modificaPersonale() {
-			System.out.println("\n1.Inserisci nuovo dipendente\n2.Elimina dipendente\n3.Aggiorna Dipendenti");
-			int scelta = 0;
-			scelta = input.nextInt();
-			switch (scelta) {
-			case 1:	System.out.println("Inserisci CID: ");
-				Integer cid = input.nextInt();
-				System.out.println("Inserisci nome: ");
-				String nome = input.next();
-				System.out.println("Inserisci cognome: ");
-				String cognome = input.next();
-				System.out.println("Inserisci grado (amministratore/dipendente): ");
-				String grado = input.next();
-				Boolean b = false;
-				if(grado.equals("amministratore") || grado.equals("Amministratore")) b = true;
-				tableD.insert(new Dipendente(cid,nome,cognome,b));
-				break;
-			case 2:	System.out.println("Inserisci il CID del dipendente che vuoi eliminare: ");
-					cid = input.nextInt();
-					Boolean finder = tableD.delete(cid);
-					if (finder == false) {
-						System.out.println("Il dipendente non esiste!");
-					}
-				break;
-			case 3: update();
-			break;
-				default:
-					System.out.println("SELEZIONA UNA DELLE 4 POSSIBILITA'");
-					break;
-			
+
 		}
-			
-			
-				
-			
-}
-		public static void update() {
-			String nome = null;
-			String cognome = null;
-			Boolean degree = null;
-			String grado = null;
-			String choice;
+	}
+
+	public static void modificaPersonale() {
+		System.out.println("\n1.Inserisci nuovo dipendente\n2.Elimina dipendente\n3.Aggiorna Dipendenti");
+		int scelta = 0;
+		scelta = input.nextInt();
+		switch (scelta) {
+		case 1:
+			System.out.println("Inserisci CID: ");
+			Integer cid = input.nextInt();
+			System.out.println("Inserisci nome: ");
+			String nome = input.next();
+			System.out.println("Inserisci cognome: ");
+			String cognome = input.next();
+			System.out.println("Inserisci grado (amministratore/dipendente): ");
+			String grado = input.next();
 			Boolean b = false;
-			System.out.println("Inserisci il Cid del dipendente che vuoi modificare");
-			Integer cid =input.nextInt();
-			Dipendente dip;
-			
-			do {
+			if (grado.equals("amministratore") || grado.equals("Amministratore"))
+				b = true;
+			tableD.insert(new Dipendente(cid, nome, cognome, b));
+			break;
+		case 2:
+			System.out.println("Inserisci il CID del dipendente che vuoi eliminare: ");
+			cid = input.nextInt();
+			Boolean finder = tableD.delete(cid);
+			if (finder == false) {
+				System.out.println("Il dipendente non esiste!");
+			}
+			break;
+		case 3:
+			update();
+			break;
+		default:
+			System.out.println("SELEZIONA UNA DELLE 4 POSSIBILITA'");
+			break;
+
+		}
+
+	}
+
+	public static void update() {
+		String nome = null;
+		String cognome = null;
+		Boolean degree = null;
+		String grado = null;
+		String choice;
+		Boolean b = false;
+		System.out.println("Inserisci il Cid del dipendente che vuoi modificare");
+		Integer cid = input.nextInt();
+		Dipendente dip;
+
+		do {
 			System.out.println("Cosa desideri modificare? (nome/cognome/grado)\nPer uscire digitare esci ");
 			choice = input.next();
 			switch (choice) {
-			case ("nome"): System.out.println("Inserisci il nuovo nome: ");
-			nome = input.next();
-			break;
-			case ("cognome"): System.out.println("Inserisci il nuovo cognome: ");
-			cognome = input.next();
-			break;
-			case ("grado"):System.out.println("Inserisci il nuovo grado: ");
-			grado = input.next();
-			if(grado.equals("amministratore") || grado.equals("Amministratore")) b = true;
-			break;
-			default: System.out.println("SELEZIONA UNA DELLE POSSIBILITA'");
-			break;			
-			}
-				
-			}while (!choice.equals("esci"));
-					
-			tableD.update(new Dipendente(cid,nome,cognome,b));
-
-			
-		}
-		
-		public static void cliente() {
-			System.out.println("1. Visualizza veicoli");
-			int choice = 0;
-			choice = input.nextInt();
-			switch (choice) {
-			case 1:
-				Collection <Veicolo>veicoli =tableV.get();
-				for(Veicolo v:veicoli) {
-					v.descrivi();
-				}
+			case ("nome"):
+				System.out.println("Inserisci il nuovo nome: ");
+				nome = input.next();
 				break;
-				default:
-					System.out.println("SCEGLI UNA DELLE SELEZIONI POSSIBILI !!!");
-					break;
+			case ("cognome"):
+				System.out.println("Inserisci il nuovo cognome: ");
+				cognome = input.next();
+				break;
+			case ("grado"):
+				System.out.println("Inserisci il nuovo grado: ");
+				grado = input.next();
+				if (grado.equals("amministratore") || grado.equals("Amministratore"))
+					b = true;
+				break;
+			default:
+				System.out.println("SELEZIONA UNA DELLE POSSIBILITA'");
+				break;
 			}
-			
+
+		} while (!choice.equals("esci"));
+
+		tableD.update(new Dipendente(cid, nome, cognome, b));
+
+	}
+
+	public static void cliente() {
+		System.out.println("1. Visualizza veicoli");
+		int choice = 0;
+		choice = input.nextInt();
+		switch (choice) {
+		case 1:
+			Collection<Veicolo> veicoli = tableV.get();
+			for (Veicolo v : veicoli) {
+				v.descrivi();
+			}
+			break;
+		default:
+			System.out.println("SCEGLI UNA DELLE SELEZIONI POSSIBILI !!!");
+			break;
 		}
-		
+
+	}
+
 }
